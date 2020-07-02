274617b3676d301b602624a36bc72b2b3c57ac1a
Nick Radonic <big.rad@gmail.com>
2020-07-02 11:49:41 -0400

    added position tracing - last 20 states, reactivated roatation

--- src/main/java/TopLevel.java
+++ src/main/java/TopLevel.java
@@ -3,20 +3,34 @@ import java.util.concurrent.TimeUnit;
 public class TopLevel {
 
     public static void main(String[] args) throws InterruptedException {
-        Vectors vectors = new Vectors(128);
-        VectorSphereDisplay vectorSphereDisplay = new VectorSphereDisplay(vectors);
+        Vectors vectors = new Vectors(50);
+        Vectors accumulatedVectors = new Vectors(0);
 
-        Double angle = 0.1;
-        for (int i = 1; i<500; i++) {
-            TimeUnit.MILLISECONDS.sleep(500);
+        VectorSphereDisplay vectorSphereDisplay = new VectorSphereDisplay(accumulatedVectors);
+        Double angle = 0.0;
+        for (int i = 1; i < 1000; i++) {
+            TimeUnit.MILLISECONDS.sleep(100);
 
             AdjustVectorsByRepulsion.rotateAroundZ(angle);
 
             AdjustVectorsByRepulsion.moveThem(vectors);
             Double minimumAngle = Spread.minimumAngle(vectors);
+            copyAllVectorsToDisplayBlock(vectors, accumulatedVectors);
             vectorSphereDisplay.annotateTitle(minimumAngle.toString());
             vectorSphereDisplay.repaint();
 
+        }
+    }
+
+    private static void copyAllVectorsToDisplayBlock(Vectors v1, Vectors v2) {
+        for (ArrowVector arrowVector : v1) {
+            v2.add(new ArrowVector(arrowVector));
+        }
+        if (v2.size() > 20 * v1.size()) {
+            int k = (int) Math.min(v1.size(), v2.size());
+            for (int i = 0; i < k; i++) {
+                v2.remove(0);
+            }
         }
     }
 }
